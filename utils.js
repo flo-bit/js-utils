60698269da3aceb053c398f185b5c8f0c5457742
florian <45694132+flo-bit@users.noreply.github.com>
2023-01-14 22:09:33 +0100

    first commit

---
+++ utils.js
@@ -0,0 +1,96 @@
+class Utils {
+  static isDict(a) {
+    return a != undefined && a.constructor == Object;
+  }
+  static isObject(a) {
+    return Utils.isDict(a);
+  }
+  static isString(a) {
+    return typeof a === "string" || a instanceof String;
+  }
+  static isNumber(a) {
+    return typeof a === "number";
+  }
+  static isFunction(a) {
+    return typeof a === "function";
+  }
+  static isArray(a) {
+    return Array.isArray(a);
+  }
+  static isBool(a) {
+    return typeof a === "boolean";
+  }
+  static isNull(a) {
+    return a === null;
+  }
+  static isDefined(a) {
+    return a !== undefined;
+  }
+  static firstDefined(...arr) {
+    if (arr == undefined) return;
+
+    for (let element of arr) {
+      if (element != undefined) return element;
+    }
+  }
+  static deepClone(a) {
+    if (!Utils.isDict(a)) return a;
+
+    let myClone = {};
+    for (let k of Object.keys(a)) {
+      myClone[k] = Utils.deepClone(a[k]);
+    }
+    return myClone;
+  }
+
+  // merge dictionaries a and b with priority to a
+  static mergeTwoDicts(a, b) {
+    if (b == undefined) return Utils.deepClone(a);
+    if (a == undefined) return Utils.deepClone(b);
+
+    let result = Utils.deepClone(a);
+    for (let k of Object.keys(b)) {
+      if (a[k] == undefined) {
+        result[k] = Utils.deepClone(b[k]);
+      } else if (Utils.isDict(a[k]) && Utils.isDict(b[k])) {
+        result[k] = Utils.mergeDicts(a[k], b[k]);
+      }
+    }
+    return result;
+  }
+
+  static merge(...arr) {
+    let result = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      result = Utils.mergeTwoDicts(result, arr[i]);
+    }
+    return result;
+  }
+
+  /**
+   * get a number from a variable
+   * if variable is a number, return it
+   * if variable is a function, call it with settings and return the result
+   * if variable is a dictionary, return a random number between min and max
+   *
+   * @param {*} obj
+   * @param {*} settings
+   * @returns
+   */
+  static getNumber(obj, settings) {
+    if (Utils.isNumber(obj)) {
+      return obj;
+    }
+    if (Utils.isFunction(obj)) {
+      return obj(settings);
+    }
+    if (Utils.isDict(obj)) {
+      if (Utils.isNumber(obj.min) && Utils.isNumber(obj.max)) {
+        let rng = settings.rng || Math.random;
+        return obj.min + rng * (obj.max - obj.min);
+      }
+    }
+  }
+}
+
+module.exports = Utils;
---
+++ utils.test.js
@@ -0,0 +1,61 @@
+const Utils = require("./utils");
+
+test("test types", () => {
+  expect(Utils.isArray([])).toBe(true);
+  expect(Utils.isBool(true)).toBe(true);
+  expect(Utils.isBool(false)).toBe(true);
+  expect(Utils.isFunction(() => {})).toBe(true);
+  expect(Utils.isNumber(1)).toBe(true);
+  expect(Utils.isNull(null)).toBe(true);
+  expect(Utils.isDefined(1)).toBe(true);
+
+  expect(Utils.isDefined(undefined)).toBe(false);
+  expect(Utils.isDict(1)).toBe(false);
+  expect(Utils.isDict({})).toBe(true);
+
+  expect(Utils.isDict([])).toBe(false);
+  expect(Utils.isDict(() => {})).toBe(false);
+  expect(Utils.isDict(null)).toBe(false);
+  expect(Utils.isDict(undefined)).toBe(false);
+  expect(Utils.isDict(true)).toBe(false);
+
+  expect(Utils.isFunction(1)).toBe(false);
+  expect(Utils.isFunction({})).toBe(false);
+  expect(Utils.isFunction([])).toBe(false);
+  expect(Utils.isFunction(null)).toBe(false);
+  expect(Utils.isFunction(undefined)).toBe(false);
+  expect(Utils.isFunction(true)).toBe(false);
+});
+
+test("merge", () => {
+  expect(Utils.merge({ a: 1 }, { b: 2 })).toEqual({ a: 1, b: 2 });
+  expect(Utils.merge({ a: 1 }, { b: 2 }, { c: 3 })).toEqual({
+    a: 1,
+    b: 2,
+    c: 3,
+  });
+  expect(Utils.merge({ a: 1 }, { a: 2 })).toEqual({ a: 1 });
+  expect(Utils.merge({ a: 1 }, { a: 2, b: 2 }, { b: 3 })).toEqual({
+    a: 1,
+    b: 2,
+  });
+  let a = { a: 1 };
+  let b = { b: 2 };
+  let c = { c: 3 };
+  expect(Utils.merge(a, b, c)).toEqual({ a: 1, b: 2, c: 3 });
+  expect(a).toEqual({ a: 1 });
+
+  let d = { a: 1, b: { c: 2 } };
+  let cloneD = Utils.deepClone(d);
+  cloneD.b.c = 3;
+  expect(d).toEqual({ a: 1, b: { c: 2 } });
+});
+
+test("firstDefined", () => {
+  expect(Utils.firstDefined(1, 2, 3)).toEqual(1);
+  expect(Utils.firstDefined(undefined, 2, 3)).toEqual(2);
+  expect(Utils.firstDefined(undefined, undefined, 3)).toEqual(3);
+  expect(Utils.firstDefined(undefined, undefined, undefined)).toEqual(
+    undefined
+  );
+});
