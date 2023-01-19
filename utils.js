a5dbffdcb94d02acc815095325715631c9a3c0ce
florian <45694132+flo-bit@users.noreply.github.com>
2023-01-19 23:54:27 +0100

    add more functions

--- utils.js
+++ utils.js
@@ -1,9 +1,9 @@
 class Utils {
-  static isDict(a) {
+  static isObject(a) {
     return a != undefined && a.constructor == Object;
   }
-  static isObject(a) {
-    return Utils.isDict(a);
+  static isDict(a) {
+    return Utils.isObject(a);
   }
   static isString(a) {
     return typeof a === "string" || a instanceof String;
@@ -20,12 +20,24 @@ class Utils {
   static isBool(a) {
     return typeof a === "boolean";
   }
+
   static isNull(a) {
     return a === null;
   }
   static isDefined(a) {
     return a !== undefined;
   }
+
+  static isNaN(a) {
+    return Number.isNaN(a);
+  }
+
+  /**
+   * return the first defined value in a list of values
+   *
+   * @param  {...any} arr
+   * @returns {any}
+   */
   static firstDefined(...arr) {
     if (arr == undefined) return;
 
@@ -33,8 +45,15 @@ class Utils {
       if (element != undefined) return element;
     }
   }
+  /**
+   * recursively clone an object
+   *
+   * @param {object} a
+   * @returns
+   */
   static deepClone(a) {
-    if (!Utils.isDict(a)) return a;
+    if (Utils.isArray(a)) return a.map((x) => Utils.deepClone(x));
+    if (!Utils.isObject(a)) return a;
 
     let myClone = {};
     for (let k of Object.keys(a)) {
@@ -43,8 +62,15 @@ class Utils {
     return myClone;
   }
 
-  // merge dictionaries a and b with priority to a
-  static mergeTwoDicts(a, b) {
+  /**
+   *  recursively merge object a and b with priority to a
+   *
+   * @param {object} a
+   * @param {object} b
+   *
+   * @returns {object}
+   */
+  static mergeTwoObjects(a, b) {
     if (b == undefined) return Utils.deepClone(a);
     if (a == undefined) return Utils.deepClone(b);
 
@@ -52,17 +78,23 @@ class Utils {
     for (let k of Object.keys(b)) {
       if (a[k] == undefined) {
         result[k] = Utils.deepClone(b[k]);
-      } else if (Utils.isDict(a[k]) && Utils.isDict(b[k])) {
-        result[k] = Utils.mergeTwoDicts(a[k], b[k]);
+      } else if (Utils.isObject(a[k]) && Utils.isObject(b[k])) {
+        result[k] = Utils.mergeTwoObjects(a[k], b[k]);
       }
     }
     return result;
   }
 
+  /**
+   * recursively merge multiple objects with priority to earlier objects
+   *
+   * @param  {...object} arr
+   * @returns {object}
+   */
   static merge(...arr) {
     let result = arr[0];
     for (let i = 1; i < arr.length; i++) {
-      result = Utils.mergeTwoDicts(result, arr[i]);
+      result = Utils.mergeTwoObjects(result, arr[i]);
     }
     return result;
   }
@@ -71,23 +103,36 @@ class Utils {
    * get a number from a variable
    * if variable is a number, return it
    * if variable is a function, call it with settings and return the result
-   * if variable is a dictionary, return a random number between min and max
+   * if variable is a dictionary,
+   *    return a random number between min and max
+   *    return val if it is a number
+   *    call get() function with settings and return the result
    *
    * @param {*} obj
-   * @param {*} settings
-   * @returns
+   * @param {object} settings
+   * @param {number} defaultVal
+   * @returns {number}
    */
-  static getNumber(obj, settings) {
+  static getNumber(obj, settings, defaultVal) {
+    if (obj == undefined) {
+      return defaultVal;
+    }
     if (Utils.isNumber(obj)) {
       return obj;
     }
     if (Utils.isFunction(obj)) {
       return obj(settings);
     }
-    if (Utils.isDict(obj)) {
+    if (Utils.isObject(obj)) {
       if (Utils.isNumber(obj.min) && Utils.isNumber(obj.max)) {
         let rng = settings.rng || Math.random;
         return obj.min + rng() * (obj.max - obj.min);
+      }
+      if (Utils.isNumber(obj.val)) {
+        return obj.val;
+      }
+      if (Utils.isFunction(obj.get)) {
+        return obj.get(settings, obj);
       }
     }
   }
@@ -108,6 +153,111 @@ class Utils {
       promises.push(Utils.loadScript(script));
     }
     return Promise.all(promises);
+  }
+
+  /**
+   * if a is not an object, return an object with key and a as value
+   *
+   * @param {any} a
+   * @param {string} key
+   * @returns {object}
+   */
+  static turnIntoObject(a, key) {
+    if (Utils.isObject(a)) return Utils.deepClone(a);
+
+    let obj = {};
+    obj[key] = a;
+    return obj;
+  }
+  static makeObject(a, key) {
+    return Utils.turnIntoObject(a, key);
+  }
+
+  /**
+   * if a is not an object turn it into an object with a[key] = a
+   * return merge of a and b[a[key]]
+   *
+   * @param {any} a
+   * @param {object} b
+   * @param {string} key - default is "preset"
+   * @returns {object}
+   */
+  static combineObjectsWithKey(a, b, key = "preset") {
+    a = Utils.turnIntoObject(a, key);
+
+    let value = a[key];
+    if (Utils.isString(value) && Utils.isObject(b)) {
+      return Utils.merge(a, b[value]);
+    }
+    return a;
+  }
+
+  /**
+   * short version of combineObjectsWithKey
+   *
+   * if a is not an object turn it into an object with a[key] = a
+   * return merge of a and b[a[key]]
+   *
+   * @param {any} a
+   * @param {object} b
+   * @param {string} key - default is "preset"
+   * @returns {object}
+   */
+  static combine(a, b, key) {
+    return Utils.combineObjectsWithKey(a, b, key);
+  }
+
+  /**
+   * get all values of an object
+   *
+   * @param {object} obj
+   * @returns {array}
+   */
+  static values(obj) {
+    let result = [];
+    for (let k of Object.keys(obj)) {
+      if (obj[k] != undefined) result.push(obj[k]);
+    }
+    return result;
+  }
+
+  /**
+   * get all keys of an object
+   *
+   * @param {object} obj
+   * @returns {array}
+   */
+  static keys(obj) {
+    return Object.keys(obj);
+  }
+
+  /**
+   * recursively resolve functions in an object,
+   * all functions are called with params
+   *
+   * @param {object} obj
+   * @param {object} params
+   * @returns {object}
+   */
+  static resolveFunctions(obj, params) {
+    if (Utils.isFunction(obj)) {
+      return obj(params);
+    }
+    if (Utils.isObject(obj)) {
+      let result = {};
+      for (let k of Object.keys(obj)) {
+        result[k] = Utils.resolveFunctions(obj[k], params);
+      }
+      return result;
+    }
+    if (Utils.isArray(obj)) {
+      let result = [];
+      for (let i = 0; i < obj.length; i++) {
+        result.push(Utils.resolveFunctions(obj[i], params));
+      }
+      return result;
+    }
+    return obj;
   }
 }
 module.exports = Utils;
--- utils.test.js
+++ utils.test.js
@@ -9,6 +9,14 @@ test("test types", () => {
   expect(Utils.isNull(null)).toBe(true);
   expect(Utils.isDefined(1)).toBe(true);
 
+  expect(Utils.isArray(undefined)).toBe(false);
+  expect(Utils.isBool(undefined)).toBe(false);
+  expect(Utils.isBool(undefined)).toBe(false);
+  expect(Utils.isFunction(undefined)).toBe(false);
+  expect(Utils.isNumber(undefined)).toBe(false);
+  expect(Utils.isNull(undefined)).toBe(false);
+  expect(Utils.isDefined(undefined)).toBe(false);
+
   expect(Utils.isDefined(undefined)).toBe(false);
   expect(Utils.isDict(1)).toBe(false);
   expect(Utils.isDict({})).toBe(true);
@@ -66,6 +74,23 @@ test("deepClone", () => {
   let cloneD = Utils.deepClone(d);
   cloneD.b.c = 3;
   expect(d).toEqual({ a: 1, b: { c: 2 } });
+
+  let a = [1, 2, 3];
+  let cloneA = Utils.deepClone(a);
+  cloneA[0] = 5;
+  expect(a).toEqual([1, 2, 3]);
+
+  let b = { a: 1, b: [1, 2, 3] };
+  let cloneB = Utils.deepClone(b);
+  cloneB.b[0] = 5;
+  expect(b).toEqual({ a: 1, b: [1, 2, 3] });
+
+  let c = [{ a: 3 }, { b: 4, c: { d: 5 } }, { e: [1, 2, { f: 6 }] }];
+  let cloneC = Utils.deepClone(c);
+  cloneC[2].e[2].f = 7;
+  cloneC[1].b = 3;
+  cloneC[1].c.d = 1;
+  expect(c).toEqual([{ a: 3 }, { b: 4, c: { d: 5 } }, { e: [1, 2, { f: 6 }] }]);
 });
 
 test("firstDefined", () => {
@@ -75,4 +100,79 @@ test("firstDefined", () => {
   expect(Utils.firstDefined(undefined, undefined, undefined)).toEqual(
     undefined
   );
+});
+
+test("turnIntoObject", () => {
+  expect(Utils.turnIntoObject("a", "test")).toEqual({ test: "a" });
+  expect(Utils.turnIntoObject(5, "test")).toEqual({ test: 5 });
+  expect(Utils.turnIntoObject(false, "test")).toEqual({ test: false });
+
+  expect(Utils.turnIntoObject({}, "test")).toEqual({});
+  let a = { a: 1 };
+  let b = Utils.turnIntoObject(a, "test");
+  expect(b).toEqual(a);
+  b.a = 2;
+  expect(a).toEqual({ a: 1 });
+});
+
+test("combineObjects", () => {
+  expect(
+    Utils.combineObjectsWithKey({ a: 1, test: "b" }, { b: { c: 17 } }, "test")
+  ).toEqual({ a: 1, c: 17, test: "b" });
+
+  expect(Utils.combineObjectsWithKey("b", { b: { c: 17 } }, "test")).toEqual({
+    c: 17,
+    test: "b",
+  });
+
+  expect(Utils.combineObjectsWithKey("b", { b: { c: 17 } })).toEqual({
+    c: 17,
+    preset: "b",
+  });
+
+  expect(
+    Utils.combineObjectsWithKey(
+      { preset: "b", test: 7 },
+      { b: { test: 17, hi: "hello" } }
+    )
+  ).toEqual({ preset: "b", test: 7, hi: "hello" });
+
+  expect(Utils.combine(undefined, undefined)).toEqual({ preset: undefined });
+  expect(Utils.combine(undefined, { a: 1 })).toEqual({
+    preset: undefined,
+  });
+
+  expect(Utils.combine({ a: 1 }, undefined)).toEqual({
+    a: 1,
+    preset: undefined,
+  });
+});
+
+test("resolveFunctions", () => {
+  expect(Utils.resolveFunctions({ a: 1, b: () => 2 })).toEqual({ a: 1, b: 2 });
+  expect(Utils.resolveFunctions({ a: 1, b: (n) => 2 * n }, 3)).toEqual({
+    a: 1,
+    b: 6,
+  });
+  expect(
+    Utils.resolveFunctions((n) => {
+      return { a: n, b: n * 2 };
+    }, 3)
+  ).toEqual({ a: 3, b: 6 });
+
+  expect(Utils.resolveFunctions({ a: { b: () => 2 } })).toEqual({
+    a: { b: 2 },
+  });
+  expect(Utils.resolveFunctions(undefined, 3)).toEqual(undefined);
+  expect(Utils.resolveFunctions("test")).toEqual("test");
+  expect(Utils.resolveFunctions(3)).toEqual(3);
+  expect(Utils.resolveFunctions(true)).toEqual(true);
+  expect(Utils.resolveFunctions([() => 4, () => 5])).toEqual([4, 5]);
+  expect(Utils.resolveFunctions([() => 4, 5])).toEqual([4, 5]);
+  expect(
+    Utils.resolveFunctions(
+      [{ c: (n) => 3 * n, b: [1, () => 2, 3] }, () => 5],
+      1
+    )
+  ).toEqual([{ c: 3, b: [1, 2, 3] }, 5]);
 });
